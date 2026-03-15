534bf7357dbb307d226e9ab341d77eacc0d159f4
kamarlaylatt <kamarehtha96@gmail.com>
2026-03-15 14:06:20 +0700

    Use shared Prisma instance in auth modules

--- lib/auth/admin.ts
+++ lib/auth/admin.ts
@@ -1,14 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaPg } from '@prisma/adapter-pg'
 import { expo } from "@better-auth/expo";
-// If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "../../generated/prisma/client";
-
-const connectionString = `${process.env.DATABASE_URL}`
-const adapter = new PrismaPg({ connectionString })
-
-const prisma = new PrismaClient({adapter});
+import { prisma } from "../../lib/prisma";
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql",
